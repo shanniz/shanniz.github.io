c3304dacbb87b753603936563b7b7b6b1535407c
shanniz <zeeshan_nizamani5@hotmail.com>
2019-01-29 17:52:04 +0500

    inheritance project c#

---
+++ courses/visualprogramming/Inheritance/Inheritance/Triangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inheritance
+{
+    class Triangle: TwoDShape
+    {
+        public Triangle()  : base(7) {            
+            System.IO.File.AppendAllText("logs.txt",
+                "\n derived class constructor called at " + DateTime.Now.ToString());
+        }
+    }
+}
